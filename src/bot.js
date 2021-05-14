c796d14fdd4f4a0d71247e3b19caf9a67a294a09
Arthur Melton <amtitan@icloud.com>
2021-05-14 12:35:36 -0400

    init

---
+++ src/bot.js
@@ -0,0 +1,14 @@
+const request = require("request");
+require("dotenv").config();
+const {Client} = require("discord.js");
+const Prefix = "SR!";
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`${client.user.tag} bot is on`);
+    client.user.setActivity(`${Prefix}help`, {
+        type: `WATCHING`
+    })
+})
+
+client.login(process.env.DISCORD_TOKEN)
